8f746b0113c4c88e241deb3341e5843c9435fb94
dean <draccusfly@hotmail.com>
2014-07-22 17:26:45 +0100

    user spec rb updates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Dean Wheatley", email: "draccusfly@hotmail.com")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
  
 
@@ -31,10 +31,21 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
-      addressed.each do |invalid_address|
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
+      end
+    end
+  end  
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
       end
     end
   end 
